bc5306ddcfb815558b43303aca48f23075c64081
hokou <30932790+hokou@users.noreply.github.com>
2020-12-23 08:53:26 +0800

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+function ShowMobileMenu(){
+    let mobileMenu = document.querySelector("#mobile-menu");
+    mobileMenu.style.display = "block";
+}
+
+function CloseMobileMenu(){
+    let mobileMenu = document.querySelector("#mobile-menu");
+    mobileMenu.style.display = "none";
+}
